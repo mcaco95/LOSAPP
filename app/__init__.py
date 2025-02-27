ba331fcf0fd6edaaa428fe4f8d792aa7569be2d4
mcaco95 <152946566+mcaco95@users.noreply.github.com>
2025-02-27 14:41:56 -0500

    add Flask-Dance dependency to requirements.txt3

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
-from flask_oauthlib.client import OAuth
 from werkzeug.middleware.proxy_fix import ProxyFix
 from .config import Config
 
@@ -15,7 +14,6 @@ login_manager.login_view = 'auth.login'
 mail = Mail()
 migrate = Migrate()
 csrf = CSRFProtect()
-oauth_client = OAuth()
 
 def create_app():
     app = Flask(__name__)
@@ -30,7 +28,6 @@ def create_app():
     mail.init_app(app)
     migrate.init_app(app, db)
     csrf.init_app(app)
-    oauth_client.init_app(app)
 
     with app.app_context():
         # Import models and routes
