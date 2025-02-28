e2a1d449aa6a58b3485ccd807dc5d84d145a6b3c
mcaco95 <152946566+mcaco95@users.noreply.github.com>
2025-02-28 07:36:32 -0500

    cleanup: remove temporary password reset changes

--- app/models/user.py
+++ app/models/user.py
@@ -33,7 +33,7 @@ class User(UserMixin, db.Model):
 
     def set_password(self, password):
         """Set password."""
-        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
